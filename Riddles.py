306b7c7a0724db3abb15887ed66cb9e6b1169239
aaalonzo <aalonzo@utexas.edu>
2018-10-07 15:21:05 -0500

    created anagrams for anagrams minigame

--- Riddles.py
+++ Riddles.py
@@ -1,27 +1,40 @@
-#Riddles.py
+ #Riddles.py
 
 import csv
 import io
 import os.path
 
-GLOBAL_PATH = ""
+GLOBAL_PATH = os.getcwd()
 
 class Riddle:
 
-	def __init__(row):
+	def __init__(self, row):
 		#Row number of the CSV for the riddle or anagram or whatever, my current idea is a two column CSV with the left column being the riddle and the right column being the solution
-		riddlefile = open(GLOBAL_PATH + "\riddles.csv")
+		riddlefile = open(GLOBAL_PATH + "/riddles.csv")
 		reader = csv.reader(riddlefile)
 		for x in range(row):
 			next(riddlefile)
-		row = next(riddlefile)
-		self.riddle = row[0]
-		self.solution = row[1]
+		rowz = next(reader)
+		self.riddle = rowz[0]
+		self.solution = rowz[1]
+		riddlefile.close()
 		
-	def solutionCheck(attempt):
+	def getSolution(self, attempt):
 		if attempt == self.solution:
 			return True
 		return False
 		
-	def getRiddle():
+	def getRiddle(self):
 		return self.riddle
+
+
+	def changeRiddle(row):
+		#Row number of the CSV for the riddle or anagram or whatever, my current idea is a two column CSV with the left column being the riddle and the right column being the solution
+		riddlefile = open(GLOBAL_PATH + "/riddles.csv")
+		reader = csv.reader(riddlefile)
+		for x in range(row):
+			next(riddlefile)
+		rowz = next(reader)
+		self.riddle = rowz[0]
+		self.solution = rowz[1]
+		riddlefile.close()
